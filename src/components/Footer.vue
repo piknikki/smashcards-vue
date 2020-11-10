d44d82b60d0d02aaf392782f6c39084ae0384612
Nikki <piknik@me.com>
2020-11-10 10:15:17 -0700

    add footer biz

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="content" style="width: 60%;">
+    Created by { Nikki }, with
+    <span class="emojis" role="img" aria-label="emoji">🏳️‍🌈, 🧠,  🍩, ☕,</span> and
+    <span class="emojis heart" role="img" aria-label="emoji">❤️️</span>.
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  width: 600px;
+}
+
+.content {
+  font-size: 20px;
+  color: grey;
+  margin: 0 auto;
+  text-align: center;
+}
+.emojis {
+  font-size: 30px;
+}
+</style>
